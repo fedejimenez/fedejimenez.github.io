c9c90c8fbef1400ff52a5411e09151a8b3c3ffe0
fedejimenez <fedeejimenez@gmail.com>
2019-09-04 13:43:13 +0200

    Updates

---
+++ precache-manifest.0e4bf5f8c540b44064eb9837678d134e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b5a4b5596f8e78d131bc6c6898e6c26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fe67b84edbbab90b51c",
+    "url": "/static/css/main.a080bf81.chunk.css"
+  },
+  {
+    "revision": "fe88f9ea393ff41d1265",
+    "url": "/static/js/2.338d892a.chunk.js"
+  },
+  {
+    "revision": "1fe67b84edbbab90b51c",
+    "url": "/static/js/main.d037dfc9.chunk.js"
+  },
+  {
+    "revision": "c49018507a564c0ada45",
+    "url": "/static/js/runtime~main.b908f969.js"
+  }
+]);
