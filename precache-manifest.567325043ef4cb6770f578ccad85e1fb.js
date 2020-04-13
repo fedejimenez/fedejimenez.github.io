f5fa26c79c0fcdefa87118b8c001cab81fcc29e1
fedejimenez <fedeejimenez@gmail.com>
2020-04-13 01:41:11 +0100

    Updates

---
+++ precache-manifest.567325043ef4cb6770f578ccad85e1fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e7288d53e60665d12b2a62e97c8a90a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad3aa1d98df60411763d",
+    "url": "/static/css/main.87f82a35.chunk.css"
+  },
+  {
+    "revision": "34a8159c72a7d9bbc485",
+    "url": "/static/js/2.fdc1ec43.chunk.js"
+  },
+  {
+    "revision": "ad3aa1d98df60411763d",
+    "url": "/static/js/main.39bdecc0.chunk.js"
+  },
+  {
+    "revision": "c49018507a564c0ada45",
+    "url": "/static/js/runtime~main.b908f969.js"
+  },
+  {
+    "revision": "dac853b4345a4288a3d8ce1a7850b80e",
+    "url": "/static/media/capabilities.dac853b4.png"
+  },
+  {
+    "revision": "9903fc8c1d3ce09df1204a572cbdb693",
+    "url": "/static/media/capacidades.9903fc8c.png"
+  }
+]);
